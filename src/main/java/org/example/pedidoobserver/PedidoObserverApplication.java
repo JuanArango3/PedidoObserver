b61652c51d14af943ee9dde075c8d2f643836959
Juan <jm.arango03@gmail.com>
2025-03-29 13:53:06 -0500

    initialize PedidoObserver project with Spring Boot and Maven setup

---
+++ src/main/java/org/example/pedidoobserver/PedidoObserverApplication.java
@@ -0,0 +1,13 @@
+package org.example.pedidoobserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PedidoObserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PedidoObserverApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/pedidoobserver/PedidoObserverApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.pedidoobserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PedidoObserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
